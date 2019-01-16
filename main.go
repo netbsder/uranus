125013aa767adbc92d77ea26903d1c2f0da9f0e0
donny.zhang <donny@donny.eastsunsoft.com>
2019-01-17 05:01:27 +0800

    add hello wolrd json api

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
 		c.String(200, "hello, world")
 	})
 
+	r.GET("/helloJson", func(c *gin.Context) {
+		c.JSON(200, gin.H{"code": 0, "msg": "", "body": "hello, world"})
+	})
+
 	r.Run(":9800") // listen and serve on 0.0.0.0:9800
 }
